2c084f3493b8d16b402cf4bbfc6bd12b8539cf72
yms1080 <yanelly.marie.sandheinrich@gmail.com>
2021-12-06 18:41:18 -0500

    defining the square class
    my code for creating a class for a square shape

---
+++ square.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+class square {
+    int side;
+    
+public:
+    square(int side)
+    string build();
+    void printsquare();
+};
+
+square::square(){
+    side = 3;
+}
+
+string square::build(){
+    string output = "square shape"
+
+}
+
+void square::printsquare(){
+    string printoutput = build();
+    cout << printoutput << end1:
+    
+
