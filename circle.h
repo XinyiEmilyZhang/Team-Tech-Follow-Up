5fa44a0e12b14e472da16bb2120fa71adc714aa6
alizemerchant <95660202+alizemerchant@users.noreply.github.com>
2021-12-06 18:45:54 -0500

    Create circle.h

---
+++ circle.h
@@ -0,0 +1,28 @@
+class Circle{
+    int radius;
+    
+    public;
+        Circle(int radius);
+        string build();
+        void printCircle();
+};
+
+Circle::Circle(){
+    radius = 1;
+}
+
+Circle::Circle(int r){
+    radius = r;
+    
+}
+
+string Circle::build(){
+    string rslt = "This is a circle";
+    
+}
+
+void Circle::printCircle(){
+    string printRst = build();
+    cout << printRst << endl;
+    
+}
